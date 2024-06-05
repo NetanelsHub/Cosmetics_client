25914765b140d6f1734ef74a8bf600802e638146
yossiav1977.gmail.com <yossiav1977@gmail.com>
2024-06-05 18:49:01 +0300

    Preset for paypal

---
+++ src/component/common/Paypal.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { PayPalButtons } from "@paypal/react-paypal-js";
+import axios from "axios";
+
+function Paypal() {
+  const createOrder = async (data) => {
+    // Order is created on the server and the order id is returned
+    const response = await axios({
+      url: "http://localhost:3000/payment/create-order",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // data:JSON.stringify({cart})
+    });
+    console.log(response.data);
+    return response.data.orderId;
+  };
+
+  const onApprove = async (data) => {
+    console.log(data);
+    // Order is captured on the server and the response is returned to the browser
+  const response = await axios({
+      url: "http://localhost:3000/payment/complete-order",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({ orderId: data.orderID }),
+    });
+    console.log(response)
+    console.log(response.data.status)
+    return response.data;
+  };
+
+  return (
+    <div className="flex justify-center items-center w-full text-white">
+      <PayPalButtons
+        createOrder={(data, actions) => createOrder(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)}
+      />
+    </div>
+  );
+}
+
+export default Paypal;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import GlobalProvider from "./utils/GlobalContext.jsx"
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <PayPalScriptProvider options={{ clientId: import.meta.env.VITE_CLIENT_PAYPAL_ID , intent:"capture" , currency:"ILS"}}>
   <GlobalProvider>
     <App />
   </GlobalProvider>
+  </PayPalScriptProvider>
+  
 );
