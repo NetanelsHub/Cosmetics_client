701785e3ca459ca7d692fad1c4382ebc16fe2a54
yossi avarahmi <yossiav1977@gmail.com>
2024-06-06 09:16:24 +0300

    Set paypal

--- src/component/common/Paypal.jsx
+++ src/component/common/Paypal.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { PayPalButtons } from "@paypal/react-paypal-js";
 import axios from "axios";
 import { shoppingContext } from "../../utils/ShoppingContext";
 
 function Paypal() {
   const { purchaseOrderInfo } = useContext(shoppingContext);
-  
+
   const createOrder = async (data) => {
     // Order is created on the server and the order id is returned
     const response = await axios({
@@ -21,20 +21,20 @@ function Paypal() {
   };
 
   const onApprove = async (data) => {
-    console.log(data);
-    // Order is captured on the server and the response is returned to the browser
-  const response = await axios({
-      url: "http://localhost:3000/payment/complete-order",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: JSON.stringify({ orderId: data.orderID }),
-    });
-    console.log(response)
-    console.log(response.data.status)
-    return response.data;
-  };
+    try {
+        // Send the order ID and purchaseOrderInfo to your server
+        const response = await axios.post("http://localhost:3000/payment/complete-order", {
+            orderId: data.orderID,
+            purchaseOrderInfo: purchaseOrderInfo
+        });
+        console.log(response.data.status);
+
+        return response.data;
+    } catch (error) {
+        console.error("Error completing order:", error);
+        // Handle errors
+    }
+};
 
   return (
     <div className="flex justify-center items-center w-full text-white">
@@ -47,3 +47,19 @@ function Paypal() {
 }
 
 export default Paypal;
+
+// const onApprove = async (data) => {
+//   console.log(data);
+//   // Order is captured on the server and the response is returned to the browser
+// const response = await axios({
+//     url: "http://localhost:3000/payment/complete-order",
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     data: JSON.stringify({ orderId: data.orderID }),
+//   });
+//   console.log(response)
+//   console.log(response.data.status)
+//   return response.data;
+// };
