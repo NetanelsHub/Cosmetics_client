07f59067320533befa78972271b02a3282f2b4f7
yossiav1977.gmail.com <yossiav1977@gmail.com>
2024-06-07 13:36:31 +0300

    Set paypal and add order to db

--- src/component/common/Paypal.jsx
+++ src/component/common/Paypal.jsx
@@ -1,11 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { PayPalButtons } from "@paypal/react-paypal-js";
 import axios from "axios";
 import { shoppingContext } from "../../utils/ShoppingContext";
 
+const url = "http://localhost:3000/orders/addOrder";
+
 function Paypal() {
   const { purchaseOrderInfo } = useContext(shoppingContext);
+  console.log("total price is:",purchaseOrderInfo.total_price);
 
+  // console.log(purchaseOrderInfo.total_price)
+  // console.log("1")
   const createOrder = async (data) => {
     // Order is created on the server and the order id is returned
     const response = await axios({
@@ -14,27 +19,59 @@ function Paypal() {
       headers: {
         "Content-Type": "application/json",
       },
+      data: {
+        total_price: purchaseOrderInfo.total_price.toString(),
+      },
       // data:JSON.stringify({cart})
     });
-    console.log(response.data);
+    // console.log(response.data);
+
     return response.data.orderId;
   };
 
   const onApprove = async (data) => {
     try {
-        // Send the order ID and purchaseOrderInfo to your server
-        const response = await axios.post("http://localhost:3000/payment/complete-order", {
-            orderId: data.orderID,
-            purchaseOrderInfo: purchaseOrderInfo
-        });
-        console.log(response.data.status);
+      console.log("1");
+      // Send the order ID and purchaseOrderInfo to your server
+      const response = await axios.post(
+        "http://localhost:3000/payment/complete-order",
+        {
+          orderId: data.orderID,
+          // purchaseOrderInfo:purchaseOrderInfo// add
+        }
+      );
 
-        return response.data;
+      console.log(response.data.status);
+
+      // after i get the client pay i  insert it to db
+      if (response.data.status === "COMPLETED") {
+        console.log("Payment process is complete.");
+
+        await addOrderDb();
+      } else {
+        console.log("Payment was not completed successfully.");
+        // Handle non-completed payment cases
+      }
+
+      return response.data;
     } catch (error) {
-        console.error("Error completing order:", error);
-        // Handle errors
+      console.error("Error completing order:", error);
+      // Handle errors
     }
-};
+  };
+
+  async function addOrderDb() {
+    try {
+      console.log(" iam in add order");
+      const { data } = await axios.post(url, purchaseOrderInfo, {
+        withCredentials: true,
+      });
+      console.log("Order added to database successfully.");
+      return data;
+    } catch (error) {
+      // console.error("An error occurred while adding the product:", error);
+    }
+  }
 
   return (
     <div className="flex justify-center items-center w-full text-white">
